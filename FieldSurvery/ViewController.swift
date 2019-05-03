3809eb6f21bfa6c8a8664115afa25ae7e521d566
Randall <rjy7wb@mail.missouri.edu>
2019-05-03 16:36:32 -0500

    init

---
+++ FieldSurvery/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  FieldSurvery
+//
+//  Created by rjy7wb on 5/3/19.
+//  Copyright © 2019 rjy7wb. All rights reserved.
+//
+import UIKit
+
+class FieldSurveryDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
